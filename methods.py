4f63be45964a6d6a9bbe2335b0d660ec567ca56d
AVBurashnikov <a.v.burashnikov@gmail.com>
2024-07-16 21:29:11 +0300

    Some major code refactoring

---
+++ methods.py
@@ -0,0 +1,37 @@
+from abc import ABCMeta, abstractmethod
+from typing import Callable, Any, List
+
+class SortingMethod(metaclass=ABCMeta):
+
+    @abstractmethod
+    def run(self, sequence: list, reversed: bool=False):
+        pass
+
+class Bubble(SortingMethod):
+
+    def run(self, sequence: list, reversed: bool=False):
+        has_permute: bool = True
+
+        while has_permute:
+            has_permute = False
+            for i in range(len(sequence)-1):
+                if sequence[i] > sequence[i+1]:
+                    sequence[i], sequence[i+1] = sequence[i+1], sequence[i]
+                    has_permute = True
+                    continue
+        return sequence if not reversed else sequence[::-1]
+
+
+class Selection(SortingMethod):
+    
+    def run(self, sequence: list, reversed: bool=False):
+        for i in range(len(sequence)-1):
+            min = sequence[i]
+            min_index = i
+            for j in range(i+1, len(sequence)):
+                if sequence[j] < min:
+                    min = sequence[j]
+                    min_index = j
+            sequence[i], sequence[min_index] = sequence[min_index], sequence[i]
+        
+        return sequence if not reversed else sequence[::-1]
--- sorter.py
+++ sorter.py
@@ -1,71 +1,72 @@
-from typing import Callable, Any, List
+from methods import SortingMethod, Bubble, Selection
 
 class Sorter:
 
-    def sort(self, 
-             sequence: list, 
-             condition: Callable[[Any, Any], bool]=(lambda a, b: a > b) , 
-             method: str='bubble_sort', order: str="asc") -> list:
-        
-        if sequence is None or not sequence or not isinstance(sequence, list):
-            raise ValueError('Sequence must be specified and has a list type.')
-        
-        try:
-            return {
-                "bubble_sort": self.__bubble_sort,
-                "merge_sort": self.__merge_sort,
-                "selection_sort": self.__selection_sort,
-            }[method](sequence, condition, order)
-        
-        except KeyError:
-            raise KeyError("Unknown sort method: '%s'." % method)
-        
-    def __ordering_sequence(self, 
-                            sequence: list, 
-                            order: str) -> list:
-        if order == "asc":
-            return sequence
-        if order == "desc":
-            return sequence[::-1]
-        else:
-            raise ValueError("Unknown sort order: '%s'" % order)
+    def __init__(self, method="bubble"):
+        self.__sorter: SortingMethod = {
+            "bubble": Bubble(),
+            "selection": Selection(),
+        }[method]       
 
-    def __bubble_sort(self, 
-                      seq: list, 
-                      condition: Callable[[Any, Any], bool], 
-                      order: str) -> list:
-        has_replacement: bool = True
+    def sort(self, sequence) -> list:
+        return self.__sorter.run(sequence)
 
-        while has_replacement:
-            has_replacement = False
+    def sort_reverse(self, sequence) -> list:
+        return self.__sorter.run(sequence, reversed=True)
 
-            for i in range(len(seq)-1):
-                if condition(seq[i], seq[i+1]):
-                    seq[i], seq[i+1] = seq[i+1], seq[i]
-                    has_replacement = True
-                    continue
+    # def sort_with_condition(self, sequence, condition) -> list:
+    #     pass
+
+    # def sort_with_condition_reverse(self, sequence, condition) -> list:
+    #     pass
+
+     
+
+    # def __bubble_sort(self, 
+    #                   seq: list, 
+    #                   condition: Callable[[Any, Any], bool], 
+    #                   order: str) -> list:
+    #     has_replacement: bool = True
+
+    #     while has_replacement:
+    #         has_replacement = False
+
+    #         for i in range(len(seq)-1):
+    #             if condition(seq[i], seq[i+1]):
+    #                 seq[i], seq[i+1] = seq[i+1], seq[i]
+    #                 has_replacement = True
+    #                 continue
         
-        return self.__ordering_sequence(seq, order)
+    #     return self.__ordering_sequence(seq, order)
     
-    def __merge_sort(self, 
-                      seq: list, 
-                      condition: Callable[[Any, Any], bool], 
-                      order: str) -> list:
-        pass
+    # def __merge_sort(self, 
+    #                   seq: list, 
+    #                   condition: Callable[[Any, Any], bool], 
+    #                   order: str) -> list:
+    #     raise NotImplementedError()
 
-    def __selection_sort(self, 
-                      seq: list, 
-                      condition: Callable[[Any, Any], bool], 
-                      order: str) -> list:
-        for i in range(len(seq)-1):
-            min = seq[i]
-            min_index = i
-            for j in range(i+1, len(seq)):
-                if condition(min, seq[j]):
-                    min = seq[j]
-                    min_index = j
-            seq[i], seq[min_index] = seq[min_index], seq[i]
+    # def __selection_sort(self, 
+    #                   seq: list, 
+    #                   condition: Callable[[Any, Any], bool], 
+    #                   order: str) -> list:
+    #     for i in range(len(seq)-1):
+    #         min = seq[i]
+    #         min_index = i
+    #         for j in range(i+1, len(seq)):
+    #             if condition(min, seq[j]):
+    #                 min = seq[j]
+    #                 min_index = j
+    #         seq[i], seq[min_index] = seq[min_index], seq[i]
         
-        return self.__ordering_sequence(seq, order)
-
+    #     return self.__ordering_sequence(seq, order)
+ 
+    # def __ordering_sequence(self, 
+    #                         sequence: list, 
+    #                         order: str) -> list:
+    #     if order == "asc":
+    #         return sequence
+    #     if order == "desc":
+    #         return sequence[::-1]
+    #     else:
+    #         raise ValueError("Unknown sort order: '%s'" % order)
 
